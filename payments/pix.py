73a55fc3a6ed66a98eb5d4b4125de7f299ea403b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-06-25 21:55:51 -0300

    testing-conclusion - test: created unit test

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix():
     def __init__(self) -> None:
         pass
 
-    def create_payment(self):
+    def create_payment(self,base_dir=""):
         #Criar paagamento na instituição financeira
         bank_payment_id = str(uuid.uuid4())
 
@@ -16,7 +16,7 @@ class Pix():
         img = qrcode.make(hash_payment)
 
         #salvar a imagem como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
 
         return {
